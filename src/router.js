d80d08307722d0a42a8975395e85107d732ac21c
leo <leo42us@gmail.com>
2017-08-01 21:14:50 +0300

    update index.html styles.css index.js router.js handlers.js relates to #6

--- src/router.js
+++ src/router.js
@@ -1,10 +1,18 @@
-
+const handlers = require('./handlers');
 
 const router = (req, res) => {
-   const url = req.url;
-   if(url === '/'){
-      res.end('<h1>Hello World</h1>')
-         }
-   }
+  const url = req.url;
+  if (url === '/') {
+    handlers.handleHomeRoute(res);
+  } else if (url.indexOf('/sunset') !== -1) {
+    handlers.handleSunset(res);
+  } else if (url.indexOf('/views') !== -1) {
+    handlers.handlePublic(res, url);
+  } else {
+    res.writeHead(404, "Content-Type:text/html");
+    res.end("<h1>404 not found</h1>");
+  }
+
+}
 
 module.exports = router;
